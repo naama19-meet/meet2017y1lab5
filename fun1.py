1e9a9ca05558b68b0f55385f890fc5571cf0fa4d
naama19-meet <naama19@meet.mit.edu>
2017-07-30 13:56:44 +0300

    Stopping here

--- fun1.py
+++ fun1.py
@@ -5,11 +5,27 @@
 ##print(c)    
 
 
-def add_numbers():
+##def add_numbers():
+##    c = 0
+##    for number in range(1, 100 + 1):
+##        print(number)
+##        c = c + number
+##    print(c)
+##    return
+##
+##anwser = add_numbers()
+##print(anwser)
+
+ 
+def add_numbers(start,end):
     c = 0
-    for number in range(1, 100 + 1):
+    for number in range(start, end + 1):
         print(number)
         c = c + number
     print(c)
+    return
 
-add_numbers()
+test1 = add_numbers(1,2)
+print(test1)
+
+test3 = add_numbers(333,777)
